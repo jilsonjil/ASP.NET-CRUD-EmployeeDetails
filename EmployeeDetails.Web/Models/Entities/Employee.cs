ba907af684c62680c335215bd5ae0777badd993d
jilsonjil <jilsonjil16@gmail.com>
2024-07-10 11:26:41 +0530

    Add project files.

---
+++ EmployeeDetails.Web/Models/Entities/Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace EmployeeDetails.Web.Models.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+        public string Address { get; set; }
+
+    }
+
+}
---
+++ EmployeeDetails.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmployeeDetails.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
